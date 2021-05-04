23dd0721141ccec90b2c7acb07caff87fac04591
andreanicte02 <andreanicte02@gmail.com>
2021-05-04 17:00:53 -0600

    -f

--- back/app.py
+++ back/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+import simplejson as json
+import os
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origin": "*"}})
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def check():
+    return "perros :v"
 
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=True)
+
